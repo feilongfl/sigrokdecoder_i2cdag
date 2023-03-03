cb96b41c525e3546bac62029e7135d1db10989e2
YuLong Yao <feilongphone@gmail.com>
2023-03-04 06:39:22 +0800

    pytest: add log

--- i2c_dag/dag_object_test.py
+++ i2c_dag/dag_object_test.py
@@ -1,1 +1,11 @@
 from .dag_object import DAGObject
+import os
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def test_dag():
+    file = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'dag_demo.yml')
+    LOGGER.info(DAGObject(file=file))
